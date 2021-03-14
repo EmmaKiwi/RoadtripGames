9e00af82b7d3f09ae099da907bf25438aff182cf
EmmaKiwi <e-caunter@mercari.com>
2021-03-15 00:06:55 +0900

    add animation

--- app/src/main/java/com/mercari/roadtripgames/numberplate/NumberPlateActivity.kt
+++ app/src/main/java/com/mercari/roadtripgames/numberplate/NumberPlateActivity.kt
@@ -1,6 +1,8 @@
 package com.mercari.roadtripgames.numberplate
 
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
@@ -8,6 +10,8 @@ import com.mercari.roadtripgames.R
 import com.mercari.roadtripgames.numberplate.di.DaggerNumberPlateComponent
 import com.mercari.roadtripgames.numberplate.di.NumberPlateModule
 import kotlinx.android.synthetic.main.activity_number_plate.*
+import nl.dionsegijn.konfetti.models.Shape
+import nl.dionsegijn.konfetti.models.Size
 import javax.inject.Inject
 
 class NumberPlateActivity  : AppCompatActivity() {
@@ -28,12 +32,46 @@ class NumberPlateActivity  : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
-        numberPlateAdapter.setOnPlateFoundListener { viewModel.onPlateFound(it) }
+        numberPlateAdapter.setOnPlateFoundListener {
+            viewModel.onPlateFound(it)
+            onPlateFound()
+        }
         number_plate_list.apply {
             adapter = numberPlateAdapter
         }
         viewModel.numberPlates.observe(this, Observer {
             numberPlateAdapter.submitList(it)
         })
+    }
+
+    private fun onPlateFound() {
+        showConfetti()
+        animateMessage()
+    }
+
+    private fun animateMessage() {
+        congratulations_layout.apply {
+            visibility = View.VISIBLE
+            postDelayed({
+                visibility = View.GONE
+            }, 1000)
+        }
+    }
+
+    private fun showConfetti() {
+        viewKonfetti.build()
+            .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+            .setDirection(0.0, 359.0)
+            .setSpeed(1f, 5f)
+            .setFadeOutEnabled(true)
+            .setTimeToLive(2000L)
+            .addShapes(Shape.Square, Shape.Circle)
+            .addSizes(Size(12))
+            .setPosition(
+                (viewKonfetti.width / 2).toFloat(),
+                (viewKonfetti.width / 2).toFloat(),
+                (viewKonfetti.height / 2).toFloat(),
+                (viewKonfetti.height / 2).toFloat())
+            .burst(3000)
     }
 }
